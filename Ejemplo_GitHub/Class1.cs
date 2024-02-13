f370c452533975daaf4427349d6e6feab88ba9ce
LUCIA SIERRA MENDOZA <luciaardilla@hotmail.com>
2024-02-12 19:32:35 -0600

    Se agregó Class1.  Form1 Modificado (se le agregó 2 botones más y 2 radiobuttons) 1er cambio realizado.

---
+++ Ejemplo_GitHub/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejemplo_GitHub
+{
+    internal class Class1
+    {
+    }
+}
